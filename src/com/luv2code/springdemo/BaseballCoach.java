92af8e3d7540de798716e7ceb8c48d3ab416f770
SeanCho <chunghwan14@gmail.com>
2019-09-07 17:17:00 -0400

    2019-09-07 : Weekend Coding

--- src/com/luv2code/springdemo/BaseballCoach.java
+++ src/com/luv2code/springdemo/BaseballCoach.java
@@ -2,11 +2,27 @@ package com.luv2code.springdemo;
 
 public class BaseballCoach implements Coach {
 
-	
+	// define a private field for the dependency
+	private FortuneService fortuneService;
+
+	// define a constructor for a dependency injection
+	public BaseballCoach(FortuneService theFortuneService) {
+
+		fortuneService = theFortuneService;
+	}
+
 	@Override
 	public String getDailyWorkOut() {
 
 		return "Spend 30 minutes on batting practise";
+
+	}
+
+	@Override
+	public String getDailyFortune() {
+
+		// use my fortuneService to get a fortune
+		return fortuneService.getFortune();
 
 	}
 
--- src/com/luv2code/springdemo/TrackCoach.java
+++ src/com/luv2code/springdemo/TrackCoach.java
@@ -2,11 +2,28 @@ package com.luv2code.springdemo;
 
 public class TrackCoach implements Coach {
 
+	private FortuneService fortuneService;
+
+	public TrackCoach(FortuneService fortuneService) {
+		super();
+		this.fortuneService = fortuneService;
+	}
+
+	public TrackCoach() {
+
+	}
+
 	@Override
 	public String getDailyWorkOut() {
 
 		return "Run a hard 5k";
 
+	}
+
+	@Override
+	public String getDailyFortune() {
+
+		return "Just Do It: " + fortuneService.getFortune();
 	}
 
 }
